baa1f5e4518f1dee2bcdcd8bc6ba87e2989821e1
maulidacy <cahyamaulida011@gmail.com>
2025-02-24 17:36:54 +0700

    update

--- Hello.java
+++ Hello.java
@@ -1,5 +1,56 @@
-public class Hello {
+import java.util.Scanner; // Mengimpor kelas Scanner untuk input
+
+public class Hello { // Mendefinisikan kelas dengan nama 'Hello' nama harus sesuai dengan nama file
+
+    public static void main(String[] args) {
+        //soal1(); // Memanggil metode soal1 untuk menampilkan soal 1
+        soal2();
+    }
+
+    //=========================== Soal 1: Deklarasi dan Inisialisasi Variabel ===========================
+    public static void soal1() {
+        System.out.println("soal 1");
+
+        // Deklarasi dan Inisialisasi Variabel
+        String nama = "Maulida Cahya";
+        int usia = 15;
+        double beratBadan = 45;
+        float tinggiBadan = 160;
+        char golonganDarah = 'F';
+        boolean isMahasiswa = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("Berat Badan: " + beratBadan + " kg");
+        System.out.println("Tinggi Badan: " + tinggiBadan + " cm");
+        System.out.println("Golongan Darah: " + golonganDarah);
+        System.out.println("Mahasiswa? " + isMahasiswa);
+    }
+    //===================================================================================================
+
+
+
+    //=========================== Soal 2: Menghitung Luas Lingkaran ===========================
+    public static void soal2() { // Mendefinisikan metode soal2
+        System.out.println("soal 1");
+        try (Scanner scanner = new Scanner(System.in)) { // Menggunakan try-with-resources untuk Scanner
+            double pi = 3.14; // Mendapatkan nilai pi dari kelas Math
+            System.out.print("Masukkan jari-jari: "); // Menampilkan pesan untuk soal 2
+            
+            // Membaca input dari pengguna dan mengonversinya ke double
+            double jawaban2 = Double.parseDouble(scanner.nextLine()); 
+            
+            // Mengalikan jawaban dengan pi
+            double hasil = jawaban2 * pi; 
+            System.out.println("Hasil dari " + jawaban2 + " dikali pi (" + pi + ") adalah: " + hasil); // Menampilkan hasil
+        } // Scanner akan otomatis ditutup di sini
+    }
+    //===================================================================================================
+
+
+/*
+public class Helloo {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-}
+}*/
